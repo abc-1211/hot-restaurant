7419dfc90d13e43ed07b1d4514a86b29dcddfc69
ian <chengkwanyu@yahoo.com.hk>
2020-06-06 17:51:07 +0800

    comment for procedures added

---
+++ server.js
@@ -0,0 +1,6 @@
+// Set up the ports, the URL passing listener and dependencies
+// Set up HTML structure
+// Decide how to structure and display the data received from user through post request
+// Decide on structure of reservation database
+// Handle the GET request to display data from database
+// Set up eventlistener to trigger the post request
